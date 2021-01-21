4ce7536d29f4f2cf582733407d16a6d158ec5056
DanyHammettt <76023871+DanyHammettt@users.noreply.github.com>
2021-01-20 19:09:13 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyB4D1bQ9QxU-QG2E0fazIyj2PIGvMN1cJg",
+    authDomain: "danyhammettt.firebaseapp.com",
+    projectId: "danyhammettt",
+    storageBucket: "danyhammettt.appspot.com",
+    messagingSenderId: "500646222377",
+    appId: "1:500646222377:web:7fc91fd33bb3de1b9fda38",
+    measurementId: "G-MT68KCGGQP"
+    });
